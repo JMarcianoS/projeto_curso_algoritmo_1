8db210437a299b2f073c03e9795582e0bc362d44
JMarcianoS <josemarcianodesousa@outlook.com>
2020-12-29 09:45:56 -0300

    Primeiro commit do código

---
+++ operadores-relacionais.js
@@ -0,0 +1,27 @@
+/*
+Var
+// Seção de Declarações das variáveis 
+  nota01, nota02: real
+
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   enota01 := 10
+   nota02 := 15
+   
+   se (nota01 <> nota02) entao
+      escreval("nota 01 e diferente")
+   senao
+      escreval("nota 01 e igual")
+   fims
+*/
+
+var nota01, nota02;
+
+nota01 = 10
+nota02 = 15
+
+if (nota01 != nota02) 
+   alert("nota 01 e diferente")
+else
+   alert("nota 01 e igual")
---
+++ primeiro.js
@@ -0,0 +1,3 @@
+var nota, primeironme, segundoNome, nota2;
+nota = 100;
+alert("Resultado:" + nota);
