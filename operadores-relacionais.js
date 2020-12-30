94930fbb12bfdf309615bb6fcdfce65c2e7bf725
JMarcianoS <josemarcianodesousa@outlook.com>
2020-12-30 10:07:32 -0300

    Removido os comentários do fonte.

--- operadores-relacionais.js
+++ operadores-relacionais.js
@@ -1,20 +1,4 @@
-/*
-Var
-// Seção de Declarações das variáveis 
-  nota01, nota02: real
 
-
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-   enota01 := 10
-   nota02 := 15
-   
-   se (nota01 <> nota02) entao
-      escreval("nota 01 e diferente")
-   senao
-      escreval("nota 01 e igual")
-   fims
-*/
 
 var nota01, nota02;
 
